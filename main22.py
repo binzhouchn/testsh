f648260f50e2ff83fa7d95bb03883f1f9e910bf9
Bin Zhou <binzhouchn@gmail.com>
2019-04-22 09:24:30 +0800

    update 4/22

--- main22.py
+++ main22.py
@@ -8,13 +8,16 @@ print(os.getcwd())
 
 
 def function(func):  # 定义了一个闭包
-    def func_in():  # 闭包内的函数
+    def func_in(*args, **kwargs):  # 闭包内的函数，因为装饰器运行的实则是闭包内的函数，所以这里将需要有形参用来接收原函数的参数。
         print('这里是需要装饰的内容，就是需要添加的内容')
-        func()  # 调用实参函数。
+        func(*args, **kwargs)  # 调用实参函数，并传入一致的实参。
 
     return func_in
 
-@function
-def test():  # 需要被装饰修改的函数。
+
+@function  # 装饰器的python写法，等价于test = function(test) .
+def test():
     print('无参函数的测试')
-print(test())
+
+
+test(5，6)
