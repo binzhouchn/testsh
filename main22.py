79784f41bbc322cde4e6750cc4b4f9ea0ae66add
Bin Zhou <binzhouchn@gmail.com>
2019-04-22 09:09:08 +0800

    update 4/22

--- main22.py
+++ main22.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
 import sklearn
+import copy
 from tqdm import tqdm, tqdm_notebook
 import os
 print(os.getcwd())
 
 a = 9
 b = a
-
 print(id(a) == id(b))
 
+print('second scenario')
+
+a = [1,2,3]
+b = copy.deepcopy(a)
+print(id(a) == id(b))
 
 print('--------------------')
