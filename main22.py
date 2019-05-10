1a96319d9bbb8a2b30a290a1e4cb9b6fd6ba4361
binzhouchn <binzhouchn@gmail.com>
2019-05-10 17:27:49 +0800

    update on 5/10

--- main22.py
+++ main22.py
@@ -1,16 +1,19 @@
-import numpy as np
-import pandas as pd
-import sklearn
-import copy
-from tqdm import tqdm, tqdm_notebook
+
 import os
 print(os.getcwd())
 
 
-def work():
-	global num
-	l.acquire()  # 这里表示调用互斥锁上锁方法，如果work函数先运行l.acquire的话，那么后边的程序就不能再修改和使用变量num。直到将其解锁后才能使用。
-	for i in range(1000000):
-		num += 1
-	print('work的num是%d' % num)
-	l.release()  # 这里表示调用互斥锁解锁方法。
+class A:
+    def __init__(self):
+        pass
+
+    def ff(self, x):
+        return x + 5
+
+
+def ff(x):
+    return x ** 2
+
+
+print(ff(10))
+print(A().ff(10))
