0df139c1723be9c63213443a0c8792b4b2e62c1e
zhoubin <binzhouchn@gmail.com>
2019-05-14 12:01:20 +0800

    update on 5/14

--- main22.py
+++ main22.py
@@ -7,4 +7,6 @@ print('hello MI.')
 os.system('which python')
 
 import pandas as pd
-print(pd.__version__)
+print(pd.read_html('http://top.baidu.com/buzz?b=1&c=513&fr=topcategory_c513'))
+
+print('ceshi')
