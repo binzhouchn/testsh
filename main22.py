2dc29bf63f254846abe8eaff071a659396c7d81b
zhoubin <binzhouchn@gmail.com>
2019-05-15 10:04:51 +0800

    update on 5/15

--- main22.py
+++ main22.py
@@ -6,8 +6,4 @@ print('hello MI.')
 
 os.system('which python')
 
-import pandas as pd
-print(pd.read_html('http://top.baidu.com/buzz?b=1&c=513&fr=topcategory_c513'))
-
-print('ceshi')
-print('ceshi2')
+print('配置gitlab成功')
