c614da8a26bacc30329e0a8e5c9abfb1cd8cadaa
binzhouchn <binzhouchn@gmail.com>
2019-07-25 08:41:49 +0800

    update on 20190725

--- main22.py
+++ main22.py
@@ -6,8 +6,7 @@ import pdfplumber
 print('配置gitlab成功')
 print('--------------------------')
 
-print('浏览器push')
-print('端内push')
 
 print('extract knowledge from pdf6')
 print('定位脚注释，提取及扣除from原页面（坐标提取）')
+print('正文知识点提取')
