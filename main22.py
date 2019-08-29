a91ae2e346f83355879cf8514934a31260d51b53
binzhouchn <binzhouchn@gmail.com>
2019-08-29 11:04:02 +0800

    update on 20190829

--- main22.py
+++ main22.py
@@ -1,23 +1,36 @@
+#!/usr/bin/env python
+# encoding:utf-8
 import os
-print(os.getcwd())
-import pdfplumber
+from pyltp import SentenceSplitter
+from pyltp import Segmentor
+from pyltp import Postagger
+from pyltp import NamedEntityRecognizer
+from pyltp import Parser
 
 
-print('配置gitlab成功')
-print('--------------------------')
+class MYLTP:
+    def __init__(self, ltp_dir):
+        self.LTP_DATA_DIR = ltp_dir
+        # 分句模型
+        self.sentencesplitter = SentenceSplitter
+        # 加载分词模型
+        self.cws_model_path = os.path.join(self.LTP_DATA_DIR, 'cws.model')
+        self.segmentor = Segmentor()
+        self.segmentor.load(self.cws_model_path)
+        # 加载词性标注模型
+        self.pos_model_path = os.path.join(self.LTP_DATA_DIR, 'pos.model')  # 词性标注模型路径，模型名称为`pos.model`
+        self.postagger = Postagger()  # 初始化实例
+        self.postagger.load(self.pos_model_path)  # 加载模型
+        # 加载NER模型
+        self.ner_model_path = os.path.join(self.LTP_DATA_DIR, 'ner.model')
+        self.recognizer = NamedEntityRecognizer()  # 初始化实例
+        self.recognizer.load(self.ner_model_path)  # 加载模型
+        # 加载依存句法分析模型
+        self.par_model_path = os.path.join(LTP_DATA_DIR, 'parser.model')
+        self.parser = Parser()  # 初始化实例
+        self.parser.load(self.par_model_path)  # 加载模型
 
 
-print('extract knowledge from pdf6')
-print('定位脚注释，提取及扣除from原页面（坐标提取）')
-print('正文知识点提取')
-
-print('提取疾病知识')
-print('疾病知识提取优化')
-
-print('跑版式识别模型')
-print('xxxx')
-
-print('完成poc和详设报告')
-print('两天完成')
-print('已经完成呢个poc，接下来完成呢个详设')
-print('完成详设')
+# 实例化
+ltp_dir = '/home/flygan/zhoubin1010/ltp_data_v3.4.0'
+myltp = MYLTP(ltp_dir)
