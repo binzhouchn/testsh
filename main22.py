33e574431b28f8859ded36cc051e0ddb93d468fa
binzhouchn <binzhouchn@gmail.com>
2020-02-14 10:15:22 +0800

    update on 20200209

--- main22.py
+++ main22.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 import pika
 import json
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='10.100.129.81'))
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
+from loguru import _logger
 
 
-def callback(ch, method, properties, body):
-    _d = json.loads(body)
-    if _d['action'] == 'handle':
-        vec = _d['vector']
-        print(" [x] Received ", vec)
-    else:
-        print('do nothing')
-
-
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
-
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
-
-from elasticsearch import helpers
-
-helpers.bulk()
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(host='10.100.129.81'))
+# channel = connection.channel()
+#
+# channel.queue_declare(queue='hello')
+#
+#
+# def callback(ch, method, properties, body):
+#     _d = json.loads(body)
+#     if _d['action'] == 'handle':
+#         vec = _d['vector']
+#         print(" [x] Received ", vec)
+#     else:
+#         print('do nothing')
+#
+#
+# channel.basic_consume(
+#     queue='hello', on_message_callback=callback, auto_ack=True)
+#
+# print(' [*] Waiting for messages. To exit press CTRL+C')
+# channel.start_consuming()
+#
+# from elasticsearch import helpers
+#
+# helpers.bulk()
 # helpers功能
