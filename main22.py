01ca4fda55ad66e77c8f4788636852bd6befd2ad
binzhouchn <binzhouchn@gmail.com>
2020-03-16 18:18:01 +0800

    update on 20200223

--- main22.py
+++ main22.py
@@ -1,32 +1,5 @@
-#!/usr/bin/env python
-import pika
-import json
-from loguru import _logger
+import pandas as pd
 
-
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host='10.100.129.81'))
-# channel = connection.channel()
-#
-# channel.queue_declare(queue='hello')
-#
-#
-# def callback(ch, method, properties, body):
-#     _d = json.loads(body)
-#     if _d['action'] == 'handle':
-#         vec = _d['vector']
-#         print(" [x] Received ", vec)
-#     else:
-#         print('do nothing')
-#
-#
-# channel.basic_consume(
-#     queue='hello', on_message_callback=callback, auto_ack=True)
-#
-# print(' [*] Waiting for messages. To exit press CTRL+C')
-# channel.start_consuming()
-#
-# from elasticsearch import helpers
-#
-# helpers.bulk()
-# helpers功能
+a = pd.DataFrame()
+a.explode()
+# xxx
